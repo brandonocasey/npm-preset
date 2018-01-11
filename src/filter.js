916db90c4317bbc08299f66bd897c48b773b3e91
brandonocasey <brandonocasey@gmail.com>
2018-01-10 19:12:12 -0500

    fix: sub command stderr/stdout intercept feat: silent

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,6 @@
 const config = require('./config');
 const path = require('path');
+const intercept = require('intercept-stdout');
 
 /**
  * shorten npm-preset paths so that we dont print
@@ -36,4 +37,38 @@ const shorten = function(str) {
   return str;
 };
 
-module.exports = shorten;
+const filter = function(options) {
+  let stderrFilter = (s) => s;
+  let stdoutFilter = (s) => s;
+
+  if (!options.shorten || process.env.NPM_PRESET_NO_SHORTEN === '1') {
+    process.env.NPM_PRESET_NO_SHORTEN = '1';
+  } else {
+    // shorten stdout and stderr
+    stderrFilter = shorten;
+    stdoutFilter = shorten;
+  }
+
+  if (options.quiet || process.env.NPM_PRESET_QUIET === '1') {
+    process.env.NPM_PRESET_QUIET = '1';
+    // never print stdout
+    stdoutFilter = (s) => '';
+  }
+
+  if (options.silent || process.env.NPM_PRESET_SILENT === '1') {
+    process.env.NPM_PRESET_SILENT = '1';
+    // never print stdout
+    stdoutFilter = (s) => '';
+
+    // never print stderr
+    stderrFilter = (s) => '';
+  }
+
+  if (options.commandsOnly || process.env.NPM_PRESET_COMMANDS_ONLY === '1') {
+    process.env.NPM_PRESET_COMMANDS_ONLY = '1';
+  }
+
+  intercept(stdoutFilter, stderrFilter);
+};
+
+module.exports = filter;

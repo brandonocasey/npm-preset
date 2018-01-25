1a2e420e428254b9f20b1e9820f63a70ba0277d8
brandonocasey <brandonocasey@gmail.com>
2018-01-25 02:29:04 -0500

    ignore: add benchmark run to new version

--- scripts/version.js
+++ scripts/version.js
@@ -5,6 +5,7 @@ const pkg = require('../package.json');
 
 if (!semver.prerelease(pkg.version)) {
   process.chdir(path.resolve(__dirname, '..'));
+  execSync('npm run bench', {stdio: 'inherit'});
   execSync('conventional-changelog -p videojs -i CHANGELOG.md -s');
-  execSync('git add CHANGELOG.md');
+  execSync('git add CHANGELOG.md BENCHMARKS.md');
 }
